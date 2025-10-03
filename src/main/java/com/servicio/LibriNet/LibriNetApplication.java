704f3ec02106f57aa96e10c3c34efa109893add3
j1gnacio <j.burgos18@ufromail.cl>
2025-10-03 00:44:34 -0300

    Estructura base de Spring Boot y dependencias

---
+++ src/main/java/com/servicio/LibriNet/LibriNetApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.LibriNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibriNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibriNetApplication.class, args);
+	}
+
+}
